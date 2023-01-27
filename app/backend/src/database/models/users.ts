eafed37be02474eb3f48980f8908ea170ab59484
FranciscoVieir <franciscovieira6397@gmail.com>
2023-01-27 16:56:38 -0300

    feat: testing whether the project is evaluating

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,45 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class Users extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+Users.init(
+  {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    modelName: 'Users',
+    timestamps: false,
+  },
+);
+
+export default Users;
